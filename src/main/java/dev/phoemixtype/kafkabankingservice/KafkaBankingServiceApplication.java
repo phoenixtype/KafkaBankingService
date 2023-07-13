bea7f12eb5cec1a322651db5f3695057819109b7
Samuel Akuma <usakuma@uwaterloo.ca>
2023-07-12 23:53:30 -0400

    Initial commit

---
+++ src/main/java/dev/phoemixtype/kafkabankingservice/KafkaBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.phoemixtype.kafkabankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/phoemixtype/kafkabankingservice/KafkaBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.phoemixtype.kafkabankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
