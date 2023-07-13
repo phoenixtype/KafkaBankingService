30e5c7128c841b6e6dc1be8e736396dd84a87a54
Samuel Akuma <usakuma@uwaterloo.ca>
2023-07-13 14:03:38 -0400

    Updated config and fixed issue with producing to the CustomerBalance topic

--- src/main/java/dev/phoemixtype/kafkabankingservice/KafkaBankingServiceApplication.java
+++ src/main/java/dev/phoemixtype/kafkabankingservice/KafkaBankingServiceApplication.java
@@ -5,58 +5,30 @@ import com.ibm.gbs.schema.Customer;
 import com.ibm.gbs.schema.CustomerBalance;
 import dev.phoemixtype.kafkabankingservice.config.CustomerBalanceConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import org.apache.kafka.clients.admin.NewTopic;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.JoinWindows;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.util.Map;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class KafkaBankingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaBankingServiceApplication.class, args);
 	}
 
-	@Bean
-	NewTopic balance() {
-		return TopicBuilder.name("Balance")
-				.partitions(6)
-				.replicas(3)
-				.build();
-	}
-
-	@Bean
-	NewTopic customer() {
-		return TopicBuilder.name("Customer")
-				.partitions(6)
-				.replicas(3)
-				.build();
-	}
-
-	@Bean
-	NewTopic customerBalance() {
-		return TopicBuilder.name("CustomerBalance")
-				.partitions(6)
-				.replicas(3)
-				.build();
-	}
 
 }
 
@@ -74,14 +46,14 @@ class Consumer {
 		System.out.println("Received for balance = " + balanceConsumerRecord.value() + ", with key for balance = " + balanceConsumerRecord.key());
 	}
 
-	@KafkaListener(topics = "${kafka.customer.balance.topic}", groupId = "kafka-cloud-consumer")
+	@KafkaListener(topics = "${kafka.customer.balance.topic}", groupId = "kafka-cloud-consumer-1")
 	public void consumeCustomerBalance(ConsumerRecord<String, CustomerBalance> customerBalanceConsumerRecord) {
 		System.out.println("Received for customer balance = " + customerBalanceConsumerRecord.value() + ", with key for customer balance = " + customerBalanceConsumerRecord.key());
 	}
 }
 
 @Component
-@EnableKafkaStreams
+@RequiredArgsConstructor
 class Processor {
 
 	@Value("${kafka.customer.topic}")
@@ -98,13 +70,11 @@ class Processor {
 
 	private final CustomerBalanceConfig customerBalanceConfig;
 
-	Processor(CustomerBalanceConfig customerBalanceConfig) {
-		this.customerBalanceConfig = customerBalanceConfig;
-	}
 
-	@Autowired
+	@Bean
 	public KStream<String, CustomerBalance> process(StreamsBuilder streamsBuilder) {
 
+		// Serde-objects are used to serialize and deserialize data when reading from and writing to Kafka topics.
 		final Serde<String> stringSerde = Serdes.String();
 		final SpecificAvroSerde<Customer> customerSerde = new SpecificAvroSerde<>();
 		final SpecificAvroSerde<Balance> balanceSerde = new SpecificAvroSerde<>();
@@ -115,39 +85,41 @@ class Processor {
 		customerBalanceSerde.configure(Map.of("schema.registry.url", schemaRegistryUrl), false);
 
 		KStream<String, Customer> customerStream = streamsBuilder.stream(customerTopic, Consumed.with(stringSerde, customerSerde)).peek((key, value)
-				-> { System.out.println("Customer process : " + value);
+				-> { System.out.println("Customer process value : " + value + " key : " + key);
 		});
 		KStream<String, Balance> balanceStream = streamsBuilder.stream(balanceTopic, Consumed.with(stringSerde, balanceSerde)).peek((key, value)
-				-> { System.out.println("Balance process : " + value);
+				-> { System.out.println("Balance process value : " + value + " key : " + key);
 		});
 
-		KStream<String, CustomerBalance> customerBalanceKStream = streamsBuilder.stream(customerBalanceTopic, Consumed.with(stringSerde, customerBalanceSerde)).peek((key, value)
-				-> { System.out.println("customer balance process : " + value);
-		});
+		KStream<String, CustomerBalance> joinedStream = joinCustomerAndBalanceStreams(customerStream, balanceStream);
 
-		KStream<String, CustomerBalance> joinedStream = customerStream.join(
-				balanceStream,
-				(customer, balance) -> {
+		joinedStream.to(customerBalanceTopic, Produced.with(stringSerde, customerBalanceSerde));
+		return joinedStream;
+	}
+
+	private KStream<String, CustomerBalance> joinCustomerAndBalanceStreams(
+			KStream<String, Customer> customerStream,
+			KStream<String, Balance> balanceStream) {
+
+		KTable<String, Customer> customerTable = customerStream
+				.selectKey((key, value) -> value.getAccountId())
+				.groupByKey()
+				.reduce((oldValue, newValue) -> newValue);
+
+		KTable<String, Balance> balanceTable = balanceStream
+				.selectKey((key, value) -> value.getAccountId())
+				.groupByKey()
+				.reduce((oldValue, newValue) -> newValue);
+
+		return customerTable
+				.join(balanceTable, (customer, balance) -> {
 					CustomerBalance customerBalance = new CustomerBalance();
 					customerBalance.setAccountId(balance.getAccountId());
 					customerBalance.setCustomerId(customer.getCustomerId());
 					customerBalance.setPhoneNumber(customer.getPhoneNumber());
 					customerBalance.setBalance(balance.getBalance());
-
-					System.out.println("Joined CustomerBalance: " + customerBalance);
-
 					return customerBalance;
-				}, JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofSeconds(5))
-		);
-
-		joinedStream.foreach((key, value) -> {
-			System.out.println("Joined CustomerBalance: " + value);
-		});
-
-		joinedStream.to(customerBalanceTopic, Produced.with(stringSerde, customerBalanceSerde));
-		streamsBuilder.build();
-
-		System.out.println("Joined stream = " + customerBalanceKStream);
-		return joinedStream;
+				})
+				.toStream();
 	}
 }
--- src/main/java/dev/phoemixtype/kafkabankingservice/config/CustomerBalanceConfig.java
+++ src/main/java/dev/phoemixtype/kafkabankingservice/config/CustomerBalanceConfig.java
@@ -2,8 +2,13 @@ package dev.phoemixtype.kafkabankingservice.config;
 
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +29,16 @@ public class CustomerBalanceConfig {
     @Value("${spring.kafka.properties.schema.registry.url}")
     private String schemaRegistryUrl;
 
+    @Value("${kafka.customer.balance.topic}")
+    private String customerBalanceTopic;
+
+    @Value("${kafka.customer.topic}")
+    private String customerTopic;
+
+    @Value("${kafka.balance.topic}")
+    private String balanceTopic;
+
+
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
     public KafkaStreamsConfiguration kStreamsConfigs() {
         Map<String, Object> props = new HashMap<>();
@@ -33,5 +49,32 @@ public class CustomerBalanceConfig {
         props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
         props.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndContinueExceptionHandler.class);
         return new KafkaStreamsConfiguration(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic customerBalanceTopic() {
+        return new NewTopic(customerBalanceTopic, 1, (short) 1);
     }
 }
